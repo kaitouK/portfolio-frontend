230cc0c31f24b521f0bf6785993e8a42f3bbf22c
KaitouK <lin1015268@gmail.com>
2026-06-25 13:34:43 +0800

    build env file instead of env.production

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import "./index.css";
 import App from "./App.tsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
+console.log(
+  "=== 頂層檢查 Google ID ===",
+  import.meta.env.VITE_GOOGLE_CLIENT_ID,
+);
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
